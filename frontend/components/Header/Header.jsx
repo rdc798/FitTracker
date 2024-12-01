b25eee4030b5aebda1937360e65be0384e98af26
Ryan Carey <ryancarey798@outlook.com>
2024-11-30 21:21:35 -0500

    Initial commit

--- frontend/components/Header/Header.jsx
+++ frontend/components/Header/Header.jsx
@@ -1,77 +1,107 @@
-import './Header.css';
+import "./Header.css";
 import React, { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faBars, faTimes } from "@fortawesome/free-solid-svg-icons";
 import { faArrowRight } from "@fortawesome/free-solid-svg-icons";
-import { faSun } from '@fortawesome/free-solid-svg-icons';
+import { faSun } from "@fortawesome/free-solid-svg-icons";
 
-function Header({isOpen, setIsOpen}) {
+function Header({ isOpen, setIsOpen }) {
+  const toggleHamburger = () => {
+    setIsOpen(!isOpen);
+  };
 
-    const toggleHamburger = () => {
-        setIsOpen(!isOpen)
-    };
+  const [isDarkMode, setIsDarkMode] = useState(true);
 
-    const [ isDarkMode, setIsDarkMode ] = useState(true);
+  const toggleDarkMode = () => {
+    setIsDarkMode(!isDarkMode);
+  };
 
-    const toggleDarkMode = () => {
-        setIsDarkMode(!isDarkMode);
-    };
-
-    return(
-        <>
-        <div className='wrapperDark'>
-            <header>
-                    <nav className={`topNav ${isOpen ? "open" : ""}`}>
-                        <div>
-                            <a href="/Home" className="links logo">Home</a>
-                        </div>
-                        <div className="extraLinks">
-                            <a href="/WorkoutLog">Workout Log</a>
-                            <a href="#">Diet</a>
-                            <a href="#">Progress</a>
-                        </div>
-                        <ul>
-                            <li><FontAwesomeIcon onClick={toggleDarkMode} icon={faSun} className={`sun ${isDarkMode ? "light" : "dark"}`}/></li>
-                            <li><a href="#" className="links login desktop" >Log In</a></li>
-                            <li><a href="#" className="links register desktop" >Register</a></li>
-                            <div className={`hamburger ${isOpen ? "open" : ""}`} onClick={toggleHamburger}>
-                                <FontAwesomeIcon  className="icon" icon={isOpen ? faTimes : faBars} size="lg" />
-                            </div>
-                        </ul>
-                    </nav>
-                </header>
-                <div className="hamburgerOpenContainer">
-                    <div className={`hamburgerOpen ${isOpen ? "open" : "closed"}`}>
-                        <div className="border">
-                            <hr />
-                        </div>
-                        <div className="hamburgerLinks">
-                            <a href="/WorkoutLog">Workout Log</a>
-                            <FontAwesomeIcon icon={faArrowRight} />
-                        </div>
-                        <div className="border">
-                            <hr />
-                        </div>
-                        <div className="hamburgerLinks">
-                            <a href="#">Link</a>
-                            <FontAwesomeIcon icon={faArrowRight} />
-                        </div>
-                        <div className="border">
-                            <hr />
-                        </div>
-                        <div className="hamburgerLinks">
-                            <a href="#">Link</a>
-                            <FontAwesomeIcon icon={faArrowRight} />
-                        </div>
-                        <ul className="hamburgerOpenLinks">
-                            <li><a href="#" className="links login" >Log In</a></li>
-                            <li><a href="#" className="links register" >Register</a></li>
-                        </ul>
-                    </div>
-                </div>
-        </div>    
-        </>
-    )
+  return (
+    <>
+      <div className="wrapperDark">
+        <header>
+          <nav className={`topNav ${isOpen ? "open" : ""}`}>
+            <div>
+              <a href="/" className="links logo">
+                Home
+              </a>
+            </div>
+            <div className="extraLinks">
+              <a href="/WorkoutLog">Workout Log</a>
+              <a href="#">Diet</a>
+              <a href="#">Progress</a>
+            </div>
+            <ul>
+              <li>
+                <FontAwesomeIcon
+                  onClick={toggleDarkMode}
+                  icon={faSun}
+                  className={`sun ${isDarkMode ? "light" : "dark"}`}
+                />
+              </li>
+              <li>
+                <a href="#" className="links login desktop">
+                  Log In
+                </a>
+              </li>
+              <li>
+                <a href="#" className="links register desktop">
+                  Register
+                </a>
+              </li>
+              <div
+                className={`hamburger ${isOpen ? "open" : ""}`}
+                onClick={toggleHamburger}
+              >
+                <FontAwesomeIcon
+                  className="icon"
+                  icon={isOpen ? faTimes : faBars}
+                  size="lg"
+                />
+              </div>
+            </ul>
+          </nav>
+        </header>
+        <div className="hamburgerOpenContainer">
+          <div className={`hamburgerOpen ${isOpen ? "open" : "closed"}`}>
+            <div className="border">
+              <hr />
+            </div>
+            <div className="hamburgerLinks">
+              <a href="/WorkoutLog">Workout Log</a>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+            <div className="border">
+              <hr />
+            </div>
+            <div className="hamburgerLinks">
+              <a href="#">Link</a>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+            <div className="border">
+              <hr />
+            </div>
+            <div className="hamburgerLinks">
+              <a href="#">Link</a>
+              <FontAwesomeIcon icon={faArrowRight} />
+            </div>
+            <ul className="hamburgerOpenLinks">
+              <li>
+                <a href="#" className="links login">
+                  Log In
+                </a>
+              </li>
+              <li>
+                <a href="#" className="links register">
+                  Register
+                </a>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
 
-export default Header
+export default Header;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,54 +1,54 @@
-import './Home.css'
-import image from '../../../assets/placeholder.jpg'
+import "./Home.css";
+import image from "../../../assets/placeholder.jpg";
 
 function Home() {
-    return(
-        <>
-        <div className="wrapperDark">
-            <div className="home">
-                <div className="welcomeText">
-                    <h1>Focus on the work.</h1>
-                    <h1>Automate your workout.</h1>
-                    <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem ipsum, dolor sit amet consectetur adipisicing elit. </p>
-                </div>
-                {/* 
-                <div className="welcomeSignup">
-                    <input placeholder='Enter Your Email' value={""} onChange={""}></input>
-                    <div className="welcomeButtons">
-                        <button className="">Test</button>
-                        <button className="">Test</button>
-                    </div>
-                </div>
-                */}
-                <div className="imageShowcase">
-                    <img src={image} alt="imageShowcase"></img>
-                </div>
-            </div>
+  return (
+    <>
+      <div className="wrapperDark">
+        <div className="home">
+          <div className="welcomeText">
+            <h1>Focus on the work.</h1>
+            <h1>Automate your workout.</h1>
+            <p>
+              Lorem ipsum, dolor sit amet consectetur adipisicing elit. Lorem
+              ipsum, dolor sit amet consectetur adipisicing elit.{" "}
+            </p>
+          </div>
 
-            <div className="wrapperLight">
-            <div className="home">
-                    <div className="featureContainer">
-                        <div className="featureText">
-                            <h1>Testing 1234</h1>
-                            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
-                        </div>
-                        <div className="featureShowcase">
-                            <div className="feature">
-                                <h1>testing</h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                            <div className="feature">
-                                <h1>testing</h1>
-                                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-                
-            </div>
+          <div className="imageShowcase">
+            <img src={image} alt="imageShowcase"></img>
+          </div>
         </div>
-        </>
-    )
+
+        {/* 
+        <div className="wrapperLight">
+          <div className="home">
+            <div className="featureContainer">
+              <div className="featureText">
+                <h1>Header</h1>
+                <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit.</p>
+              </div>
+              <div className="featureShowcase">
+                <div className="feature">
+                  <h1>testing</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+                <div className="feature">
+                  <h1>testing</h1>
+                  <p>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                  </p>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+            */}
+      </div>
+    </>
+  );
 }
 
-export default Home
+export default Home;
